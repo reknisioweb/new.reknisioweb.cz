c85e83742dd83895ee6a3480d57a11df732f3a22
Martin Kopta <martin.kopta@garcon.cz>
2023-05-11 14:02:35 +0200

    WIP

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const dataPrivacy = function (action) {
+    switch (action) {
+        case "allow": // Kliknul na Povolit
+            var cookieBar = document.querySelector("[data-privacy]");
+            cookieBar.innerHTML('<small>Tento web ukládá do prohlížeče cookie první strany pro analýzy návštěvnost (<a href="https://support.google.com/analytics/topic/2919631">Google Analytics</a>). <button>Nesouhlasím</button></small>')
+            cookieBar.getElementsByTagName("button")[0].addEventListener("click", () => { dataPrivacy("allow"); })
+            gtag('consent', 'update', {
+                'analytics_storage': 'granted'
+            });
+            document.cookie = "consent=granted; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/; domain=reknisioweb.cz; secure";
+            break;
+        case "disallow": // Kliknul na Zakázat
+            var cookieBar = document.querySelector("[data-privacy]");
+            cookieBar.innerHTML('<small>Pro zlepšování kvality tohoto webu potřebujeme povolit analytiku (<a href="https://support.google.com/analytics/topic/2919631">Google Analytics</a>). <button>Povolit</button></small>')
+            cookieBar.getElementsByTagName("button")[0].addEventListener("click", () => { dataPrivacy("allow"); })
+            gtag('consent', 'update', {
+                'analytics_storage': 'denied'
+            });
+
+            const exp = new Date();
+            exp.setUTCFullYear(exp.getUTCFullYear + 1)
+
+            document.cookie = `consent=denied; expires=${exp.toUTCString()}; path=/; domain=reknisioweb.cz; secure`;
+            break;
+        default:
+            const cookies = {};
+            document.cookie.split(/\s*;\s*/).forEach((cookie) => {
+                Object.assign(cookies, JSON.parse(`{ "${cookie.split(/\s*=\s*/).join('": "')}" }`))
+            });
+            if (cookies.hasOwn(consent) && cookies.consent.toLowerCase().indexOf("denied") == 0) {
+                dataPrivacy("disallow");
+            } else {
+                gtag('consent', 'update', {
+                    'analytics_storage': 'granted'
+                });
+            }
+            break;
+    }
+}
+
+const ready = function (fn, ...args) {
+    if (document.readyState !== 'loading') {
+        fn(...arguments.shift());
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-HFFN6HTH6J');
+gtag('consent', 'default', {
+    'ad_storage': 'denied',
+    'analytics_storage': 'denied'
+});
+
+ready(dataPrivacy);
